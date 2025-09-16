f4fa4899c2c82636621f2563041cfd9e3d2872d1
hpphpro <Fullaurum@gmail.com>
2025-09-16 16:23:44 +0300

    chore: hasher initializing

--- src/backend/infra/security/hasher.py
+++ src/backend/infra/security/hasher.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
-from typing import Literal
+from collections.abc import Mapping
+from dataclasses import asdict
+from typing import Final, Literal
 
 from argon2 import Parameters, PasswordHasher
 from argon2.exceptions import VerificationError, VerifyMismatchError
@@ -14,10 +16,9 @@ from argon2.profiles import (
 from backend.infra.shared.result import as_result
 
 
-ProfileType = Literal[
-    "RFC_9106_LOW_MEMORY", "RFC_9106_HIGH_MEMORY", "CHEAPEST", "PRE_21_2", "DEFAULT"
-]
-PROFILES: dict[str, Parameters] = {
+ProfileType = Literal["RFC_9106_LOW_MEMORY", "RFC_9106_HIGH_MEMORY", "CHEAPEST", "PRE_21_2"]
+
+PROFILES: Final[Mapping[str, Parameters]] = {
     "RFC_9106_LOW_MEMORY": RFC_9106_LOW_MEMORY,
     "RFC_9106_HIGH_MEMORY": RFC_9106_HIGH_MEMORY,
     "CHEAPEST": CHEAPEST,
@@ -34,6 +35,14 @@ class Argon2:
     @classmethod
     def default(cls) -> Argon2:
         return cls(PasswordHasher())
+
+    @classmethod
+    def from_profile(cls, profile: ProfileType) -> Argon2:
+        return cls(PasswordHasher(**asdict(PROFILES[profile])))
+
+    @classmethod
+    def from_parameters(cls, parameters: Parameters) -> Argon2:
+        return cls(PasswordHasher(**asdict(parameters)))
 
     @as_result(is_async=False)
     def hash_password(self, plain: str) -> str:
